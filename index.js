0f0928211e596046bbfbf81be2819cb4a2fbdc07
Saif mhmd <saifmuhmd@saif-mhmd-2.local>
2025-02-24 16:05:52 +0530

    layout change and contact section add

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const links = [
     "projects.html",
     "blog.html",
     "about.html",
+    "contact.html",
     "gallery.html"
 ];
 
@@ -14,9 +15,17 @@ containers.forEach((container, index) => {
 });
 
 document.addEventListener("DOMContentLoaded", function() {
-    const container = document.getElementById("mobile-about");
+    const mobileAboutContainer = document.getElementById("mobile-about-container");
 
-    container.addEventListener("click", function() {
+    mobileAboutContainer.addEventListener("click", function() {
         window.location.href = "about.html"; // Change this to your target page
+    });
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    const mobileContactContainer = document.getElementById("mobile-contact-container");
+
+    mobileContactContainer.addEventListener("click", function() {
+        window.location.href = "contact.html"; // Change this to your target page
     });
 });
